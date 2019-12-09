86fe08d161dd86a32c396198b4ba6b65a22e3362
Sean Beach <sean.beach1@yahoo.com>
2019-12-09 00:09:39 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -2,3 +2,17 @@ $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'directors_database'
 require 'pp'
 
+def pretty_print_nds(nds)
+  pp nds
+end
+
+def print_first_directors_movie_titles
+  ss_movies = directors_database[0][:movies]
+  index = 0
+
+  while index < ss_movies.length do
+    titles = ss_movies[index][:title]
+    puts titles
+    index +=1
+  end
+end
